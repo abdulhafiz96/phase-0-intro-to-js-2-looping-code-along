3c3f7070e8c99476d8d4534b5766c6d9b5469e7d
Abdulhafiz <hafeez_96@hotmail.com>
2022-04-09 04:56:48 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+const messages = [];
+const array = [];
+
+function writeCards(array, event) {
+for (let i = 0; i < array.length; i++) {
+messages.push(`Thank you, ${array[i]}, for the wonderful surprise gift!`)
+}
+return messages
+}
+function countDown(number) {
+    let i = number;
+    while (i >= 0) {
+    console.log(i--)    
+    }
+    }
+    console.log(countDown(10))
